7234f986758251349a886ba7f01bd1cf1e34ad2a
IT小强xqitw.cn <360237521@qq.com>
2018-12-16 22:49:48 +0800

    静态资源设置

---
+++ template/default/assets.php
@@ -0,0 +1,16 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | 静态资源设置
+// +----------------------------------------------------------------------
+
+return [
+    'form.css' => [
+        'https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css'
+    ],
+    'form.js'  => [
+        'https://cdn.bootcss.com/jquery/3.2.1/jquery.slim.min.js',
+        'https://cdn.bootcss.com/popper.js/1.12.9/umd/popper.min.js',
+        'https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.min.js',
+    ]
+];
