dd683ac7b38ad7710f448750b8dbb27a202471dd
IT小强xqitw.cn <360237521@qq.com>
2018-12-16 22:46:09 +0800

    助手函数 完善中

--- src/helper.php
+++ src/helper.php
@@ -10,22 +10,57 @@
  *  ==================================================================
  */
 
-if (!function_exists('config')) {
+if (!function_exists('create_form')) {
+    
     /**
-     * 获取和设置配置参数
-     * @param string|array $name 参数名
-     * @param mixed $value 参数值
+     * 创建表单
+     * @param array|null $data - 表单默认数据
+     * @param int $colWidth - 表单label宽度
+     * @param string $template - 使用模板名
+     * @param array $config - 更多配置项
+     * @return \itxq\builder\FormBuilder
+     * @throws \think\Exception
+     */
+    function create_form($data = null, int $colWidth = 2, string $template = 'default', array $config = []) {
+        $config['data'] = $data;
+        $config['width'] = $colWidth;
+        $config['template'] = $template;
+        $formBuilder = new \itxq\builder\FormBuilder($config);
+        return $formBuilder;
+    }
+}
+
+if (!function_exists('builder_event_listen')) {
+    /**
+     * 动态添加行为扩展到某个标签
+     * @param  string $event 事件名称
+     * @param  mixed $listener 监听操作（或者类名）
+     * @param  bool $first 是否优先执行
+     * @return void
+     */
+    function builder_event_listen(string $event, $listener, bool $first = false): void {
+        \think\facade\Event::listen($event, $listener, $first);
+    }
+}
+
+if (!function_exists('builder_event_trigger')) {
+    /**
+     * 触发事件
+     * @param  string|object $event 事件名称
+     * @param  mixed $params 传入参数
+     * @param  bool $once 只获取一个有效返回值
      * @return mixed
      */
-    function config($name = '', $value = null) {
-        if (is_null($value) && is_string($name)) {
-            if ('.' == substr($name, -1)) {
-                return \think\facade\Config::pull(substr($name, 0, -1));
+    function builder_event_trigger($event, $params = null, bool $once = false) {
+        $event = \think\facade\Event::trigger($event, $params, $once);
+        $html = '';
+        if (is_array($event)) {
+            foreach ($event as $v) {
+                $html .= $v;
             }
-            
-            return 0 === strpos($name, '?') ? \think\facade\Config::has(substr($name, 1)) : \think\facade\Config::get($name);
+        } else {
+            $html = strval($event);
         }
-        
-        return \think\facade\Config::set($name, $value);
+        return $html;
     }
 }
